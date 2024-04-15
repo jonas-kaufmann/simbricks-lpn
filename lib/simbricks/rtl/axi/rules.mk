6ab203d0651814dc220bc83c22e45b9c6c496321
Jonas Kaufmann <jokau03@gmail.com>
2024-04-15 14:28:41 +0200

    lib/simbricks/axi: rework AXI Subordinate (Slave) adapter

--- lib/simbricks/rtl/axi/rules.mk
+++ lib/simbricks/rtl/axi/rules.mk
@@ -22,14 +22,4 @@
 
 include mk/subdir_pre.mk
 
-lib_verilator_axi := $(d)libverilator_axi.a
-
-OBJS := $(addprefix $(d),axi.o)
-
-$(lib_verilator_axi): CPPFLAGS += -Og -ggdb -fno-omit-frame-pointer #-fsanitize=address
-# $(lib_verilator_axi): LDFLAGS += -fsanitize=address -static-libasan
-$(lib_verilator_axi): $(OBJS)
-
-ALL := $(lib_verilator_axi)
-CLEAN := $(lib_verilator_axi) $(OBJS)
 include mk/subdir_post.mk
