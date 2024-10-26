7471b26c83224957ef414ef7e99e666c67368087
Jonas Kaufmann <jokau03@gmail.com>
2024-10-26 22:30:33 +0200

    images/install-vta.sh: only clone most recent commit

--- images/scripts/install-vta.sh
+++ images/scripts/install-vta.sh
@@ -31,7 +31,7 @@ apt-get -y install --no-install-recommends \
 
 # build tvm
 mkdir -p /root
-git clone --depth 10 --recursive --branch lpn https://github.com/jonas-kaufmann/tvm-simbricks.git /root/tvm
+git clone --depth 1 --recursive --branch lpn https://github.com/jonas-kaufmann/tvm-simbricks.git /root/tvm
 cd /root/tvm
 cp 3rdparty/vta-hw/config/simbricks_pci_sample.json 3rdparty/vta-hw/config/vta_config.json
 mkdir build
